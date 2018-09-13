88df295c4fd8cd9f717e88b1c69e920c7c06cfb4
Thanongkiat Tamtai <tt@20scoops.net>
2018-09-13 14:09:08 +0700

    Edit # Import Dataset
    Co-Authored-By: holywisdom <holywisdom@users.noreply.github.com>

--- pre-process.py
+++ pre-process.py
@@ -4,15 +4,18 @@ import pandas as pd
 
 # Import Dataset
 
-Advertisement_log_dataset = pd.read_csv('ad_log.csv')
-Credit_card_transaction_dataset = pd.read_csv('cc_txn.csv')
-Debit_card_dataset = pd.read_csv('dc_txn.csv')
-Customer_demographic_dataset = pd.read_csv('demo.csv')
-Action_history_dataset = pd.read_csv('events.csv')
-Purchase_history_dataset = pd.read_csv('purchase.csv')
-Saving_balance_dataset = pd.read_csv('sa_bal.csv')
-y_index_dataset = pd.read_csv('y_index.csv')
-customer_index_dataset = pd.read_csv('y_train.csv')
+advertisement_log_dataset = pd.read_csv('ad_log.csv')
+credit_card_transaction_dataset = pd.read_csv('cc_txn.csv')
+debit_card_dataset = pd.read_csv('dc_txn.csv')
+customer_demographic_dataset = pd.read_csv('demo.csv')
+action_history_dataset = pd.read_csv('events.csv')
+purchase_history_dataset = pd.read_csv('purchase.csv')
+saving_balance_dataset = pd.read_csv('sa_bal.csv')
+customer_index_dataset = pd.read_csv('y_index.csv')
+training_dataset = pd.read_csv('y_train.csv')
+
+
+# Dataset Structure
 
 
 # Rename columns
